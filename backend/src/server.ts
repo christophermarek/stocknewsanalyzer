28d22b7d2ec5e9a7d2ba40e113691a06e938b806
christophermarek <christophermarek42@gmail.com>
2021-05-10 12:06:32 -0400

    Server running now

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
 import express, { Express } from 'express'
+import * as dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import bnnmarketcallRoutes from './routes'
+
+dotenv.config();
+console.log("dotenv", dotenv);
 
 const app: Express = express()
 
@@ -10,7 +14,7 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors())
 app.use(bnnmarketcallRoutes)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb+srv://chris:${process.env.MONGO_PASSWORD}@cluster0.vgjbs.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set('useFindAndModify', false)
 
