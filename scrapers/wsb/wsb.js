eefcb6be6f4ed2db60a3ca1b6559cc2aee5a8b0a
christophermarek <christophermarek42@gmail.com>
2021-05-23 00:11:18 -0400

    fetching all comments properly with new api

--- scrapers/wsb/wsb.js
+++ scrapers/wsb/wsb.js
@@ -41,11 +41,43 @@ async function getComments(commentIds){
         clientSecret: process.env.REDDIT_CLIENT_SECRET,
         refreshToken: process.env.REDDIT_REFRESH_TOKEN
     });
+    
+    for(let i = 0; i < commentIds.length; i++){
+        let comment = await r.getComment(commentIds[i]).body;
+        console.log(comment);
+        console.log(i);
+    }
 
     //ok need to find what to actually set limit and depth too
     //I feel like too much depth might be useless since certain comments 
-    let comments = await r.getSubmission('nhoua8').expandReplies({limit: 20000, depth: 1});
-    return comments;
+    
+    //return comments;
+}
+
+async function getCommentIds(){
+    const { data } = await axios.get(
+        'https://api.pushshift.io/reddit/submission/comment_ids/nhoua8'
+    );
+
+    return data.data;
+}
+
+async function getCommentText(commentIdStrings){
+
+    let commentTextList = [];
+    for(let i = 0; i < commentIdStrings.length; i++){
+        const { data } = await axios.get(
+            `https://api.pushshift.io/reddit/comment/search?ids=${commentIdStrings[i]}`
+        );
+        
+        //clean data
+        for(let j = 0; j < data.data.length; j++){
+            commentTextList.push({createdAt: data.data[i].created_utc, text: data.data[i].body});
+        }
+
+    }
+
+    return commentTextList;
 }
 
 async function wsbScraper(){
@@ -56,15 +88,40 @@ async function wsbScraper(){
 
     // Create a new snoowrap requester with OAuth credentials.
 
+    let commentIds = await getCommentIds();
+
+    //the comment id string must be less than 2048 per request or the server wont be able to handle it
+    //i got a 414 error and im just assuming 2048 because that is url length limit of most browsers
+    let commentIdStrings = []; 
+    let commentIdString = "";
     
+    for(let i = 0; i < commentIds.length; i++){
+        //max url length is 2048, most of the time
+        if(commentIdString.length > 2040){
+            //del last comma
+            if(commentIdString.charAt(commentIdString.length - 1) == ","){
+                commentIdString = commentIdString.slice(0, -1);
+            }
+            commentIdStrings.push(commentIdString);
+            commentIdString = "";
+        }
+
+        //no commma last line
+        if(i == commentIds.length - 1){
+            commentIdString += commentIds[i];
+        }else{
+            commentIdString += commentIds[i] + ",";
+        }
+    }
+
+    let commentTextList = await getCommentText(commentIdStrings);
     //this site gets us all the comment id's, we need this because the reddit api will 
     //block us because there are too many comments to request at once.
-    const { data } = await axios.get(
-        'https://api.pushshift.io/reddit/submission/comment_ids/nhoua8'
-    );
+    console.log(commentTextList.length);
 
+    //console.log(data.data);
     // Extracting every comment on a thread
-    let comments = await getComments(data.data);
+    //let comments = await getComments(data.data);
 
     //console.log(comments.comments);
 
