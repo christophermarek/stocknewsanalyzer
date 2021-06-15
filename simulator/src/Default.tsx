1a8a88f4d22129357e11e6f85a79c92e1ac0ba45
christophermarek <christophermarek42@gmail.com>
2021-06-14 22:16:20 -0400

    added market page view

--- simulator/src/Default.tsx
+++ simulator/src/Default.tsx
@@ -1,6 +1,7 @@
 import React, { forwardRef, useEffect, useState } from "react";
 
 import { getHistoricalPrices } from "./API"
+import CoinGecko from 'coingecko-api';
 
 //SHOULD PROBABLY JUST RENAME THIS TO MARKET SUMMARY PAGE
 
@@ -12,7 +13,11 @@ function Default() {
   const [nasdaq, setNasdaq] = useState<Array<yahooStockHistoricalPrices>>([]);
   const [cadUsd, setcadUsd] = useState<Array<yahooStockHistoricalPrices>>([]);
   const [oil, setOil] = useState<Array<yahooStockHistoricalPrices>>([]);
+  const [btc, setBtc] = useState<Array<yahooStockHistoricalPrices>>([]);
+  const [eth, setEth] = useState<Array<yahooStockHistoricalPrices>>([]);
+
   const [dataFetched, setDataFetched] = useState<boolean>(false);
+  const CoinGeckoClient: any = new CoinGecko();
 
   const fetchSummary = (name: string, _startMonth: string, _startDay: string, _startYear: string, _endMonth: string, _endDay: string, _endYear: string, _ticker: string, _frequency: string): void => {
     //to try and stop too many api calls by accident
@@ -44,6 +49,31 @@ function Default() {
     }
 
   }
+  let fetchCryptoPrices = async () => {
+    let today = new Date();
+    let yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+
+
+    let btcData = await CoinGeckoClient.coins.fetchMarketChart('bitcoin', {
+      days: 1,
+      interval: 'daily',
+      vs_currency: 'usd',
+    });
+
+    let etherData = await CoinGeckoClient.coins.fetchMarketChart('ethereum', {
+      days: 1,
+      interval: 'daily',
+      vs_currency: 'usd',
+    });
+
+
+    setBtc(btcData.data);
+    setEth(etherData.data);
+
+  };
+
 
   //to calculate daily percent change we just need to fetch the price from yesterday, today
   if ((snp500.length < 1 && dataFetched == false)) {
@@ -58,9 +88,13 @@ function Default() {
       fetchSummary(tickersToFetch[i], `${yesterday.getMonth()}`, `${yesterday.getDate()}`, `${yesterday.getFullYear()}`, `${today.getMonth()}`, `${today.getDate() + 1}`, `${today.getFullYear()}`, 'none', '1d')
     }
 
+    //fetch crypto
+    fetchCryptoPrices();
+
     //to prevent too many calls to my api server on rerenders
     setDataFetched(true);
   }
+
 
   const renderSummary = (summaryData: yahooStockHistoricalPrices[], summaryTitle: string) => {
     let today = Number(summaryData[0].close);
@@ -77,12 +111,34 @@ function Default() {
     )
   }
 
+  const renderCryptoSummary = (cryptoData: any, summaryTitle: string) => {
+
+    if (cryptoData.prices != undefined) {
+      console.log(cryptoData.prices[0][1]);
+      let today = Number(cryptoData.prices[0][1]);
+      let yesterday = Number(cryptoData.prices[1][1]);
+      let change = ((today - yesterday) / yesterday) * 100;
+
+      
+      return (
+        <div className="summaryContainer">
+          <p>{summaryTitle}</p>
+          <p>{`Price: ${cryptoData.prices[1][1]}`}</p>
+          <p>{`Market Cap: ${cryptoData.market_caps[1][1]} volume: ${cryptoData.total_volumes[1][1].volume}`}</p>
+          <p className={change > 0 ? "green" : "red"}>24hr Change: {change.toFixed(2)} %</p>
+        </div>
+      )
+      
+    }
+
+  }
+
   return (
     <>
       <p>(Reminder)If it is monday the 24 hour change will be 0.00% for markets that are closed on sunday</p>
 
       <div >
-        <p className="marketTitle">US Markets</p>
+        <p className="marketTitle">US Market</p>
         <div className="marketSummaryTab">
           {snp500.length > 0 ? (
             renderSummary(snp500, 'S&P500')
@@ -106,7 +162,7 @@ function Default() {
       </div>
 
       <div >
-        <p className="marketTitle">Canadian Markets</p>
+        <p className="marketTitle">Canadian Market</p>
         <div className="marketSummaryTab">
           {tsxsnp.length > 0 ? (
             renderSummary(tsxsnp, 'TSX/SNP')
@@ -116,6 +172,24 @@ function Default() {
           }
           {cadUsd.length > 0 ? (
             renderSummary(cadUsd, 'CAD/USD')
+          ) : (
+            <p>Loading</p>
+          )
+          }
+        </div>
+      </div>
+
+      <div >
+        <p className="marketTitle">Crypto  Market</p>
+        <div className="marketSummaryTab">
+          {btc != undefined ? (
+            renderCryptoSummary(btc, 'BTC/USD')
+          ) : (
+            <p>Loading</p>
+          )
+          }
+          {eth != undefined ? (
+            renderCryptoSummary(eth, 'ETH/USD')
           ) : (
             <p>Loading</p>
           )
